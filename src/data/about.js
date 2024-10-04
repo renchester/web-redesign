1950cb05b7aa755708ea66099deee23cc9dc5a2e
Renchester Ramos <renchesterjramos@gmail.com>
2024-10-04 22:33:36 +0800

    Create data files for reference

---
+++ src/data/about.js
@@ -0,0 +1,62 @@
+export const aboutDetails = [
+  {
+    title: 'E-commerce, Website, and Mobile App',
+    description:
+      'We work with brands to develop innovative solutions that will help reach their business goals through contemporary website design and development, mobile applications, microsite development, and systems that will fast track and digitize operations for their businesses.\nWe use the latest technologies available from different platforms and bring them together to create an extensive platform where people can experience brands and get to know them in an interactive way.',
+    key: 'App',
+  },
+  {
+    title: 'Performance Marketing',
+    description:
+      'We assist brands in determining the optimal media spending for their online marketing campaigns in social media and e-commerce platforms to increase their sales in the lowest cost and most efficient way possible.',
+    key: 'PerformanceMarketing',
+  },
+  {
+    title: 'Chatbot',
+    description:
+      'We help brands stay connected and engaged with their target audience by developing conversational chatbots and adding them as an interactive way for brands to talk to their consumers anytime of the day.\nWe automate and personalize each chatbot depending on the objective of the brand and what their consumers need from them. Having chatbots on brands’ Facebook Pages and websites can provide instant and real time customer service to more people at the same time, generate sales through e-commerce, and simply making the experience easier for consumers to talk to them.',
+    key: 'Chatbot',
+  },
+  {
+    title: 'Webinar Production and Management',
+    description:
+      'In this "new normal", events and other brand efforts are turned virtually with the use of Facebook Live or YouTube Live and other streaming platforms. Let us help your brand be ready as we produce and manage your live webcast that is customized to your brand\';s campaign and image.Our broadcast producers will be hands on in making sure that your broadcast content, live or pre-recorded, will have a smooth flow when it\'s time go live.',
+    key: 'Webinar',
+  },
+  {
+    title: 'Digital Transformation',
+    description:
+      'We help brands transition digitally into the "new normal" by assisting them on the best practices when it comes to running digital ad campaigns.\nWe help brands achieve growth in the digital space by making sure that marketing strategies will build brand relevance and good customer experience digitally.',
+    key: 'DigitalTransformation',
+  },
+  {
+    title: 'Social Media Marketing',
+    description:
+      'We work hand in hand with brands to create a strategic social media campaign fitted to reach their overall objectives that will start online and create an impact offline.\nWe bridge the gap between brands and their target audience by listening to what they say and develop engaging content to effectively convey each brands’ messages and create a conversation that will spark interest to their target audiences',
+    key: 'SocialMedia',
+  },
+  {
+    title: 'Digital Media Planning and Placement',
+    description:
+      'We strategically place ads in platforms that we know will work best for brands based on their objectives and materials to reach the right audience and generate measurable results.\nWorking closely with Facebook and Google will give us their up-to-date media strategies and tools that will help maximize results for our brands who are using their platforms.',
+    key: 'DigitalPlanning',
+  },
+  {
+    title: 'Influencer Marketing',
+    description:
+      'Having a network of social media influencers who are known credible in different fields like food, fashion, lifestyle, and vlogging, we make sure to work with the right set of influential people who are tailored-fit to represent brands and what they believe in.\nUsing this strategy will help brands create a positive image to their target audience as it creates feedback and conversation from people who follow these chosen influencers.',
+    key: 'InfluencerMarketing',
+  },
+  {
+    title: 'Creatives Development',
+    description:
+      'We bring life to brands and our ideas by developing creative, innovative, and striking designs in the forms of branding, logo creation, marketing collaterals, motion graphics, animated videos, and digital ad layouts for Facebook, Google Display, and other digital platforms.\nWe ensure that all designs go through a creative process from brainstorming based on each brands’ goals to the actual production of each material that will represent the brands’ identities to their target audience.',
+    key: 'Creatives',
+  },
+  {
+    title: 'Video Conceptualization and Production',
+    description:
+      'We capture every brands’ vision and make them into delightful pieces of content. We tell stories through photo, video, and sound production.\nFrom producing long and short videos to creating branded jingles, we ensure that every product will tell what the brand is all about.',
+    key: 'VideoProduction',
+  },
+];
---
+++ src/data/clients.js
@@ -0,0 +1,222 @@
+export const clients = [
+  {
+    name: 'Department of Tourism',
+    img: '/logos/dot.png',
+    showTitle: true,
+  },
+  {
+    name: 'Silka',
+    img: '/logos/silka.png',
+    showTitle: false,
+  },
+  {
+    name: 'Biogenic',
+    img: '/logos/biogenic.png',
+    showTitle: false,
+  },
+  {
+    name: 'Juicy Cologne',
+    img: '/logos/juicy.png',
+    showTitle: false,
+  },
+  {
+    name: 'Kopiko',
+    img: '/logos/kopiko.png',
+    showTitle: false,
+  },
+  {
+    name: 'Cobra',
+    img: '/logos/cobra.png',
+    showTitle: false,
+  },
+  {
+    name: 'Vitamilk',
+    img: '/logos/vitamilk.png',
+    showTitle: false,
+  },
+  {
+    name: '3M',
+    img: '/logos/3m.png',
+    showTitle: false,
+  },
+  {
+    name: 'Bambini',
+    img: '/logos/bambini.png',
+    showTitle: false,
+  },
+  {
+    name: 'ChocoMucho',
+    img: '/logos/chocomucho.png',
+    showTitle: true,
+  },
+  {
+    name: 'Rebisco',
+    img: '/logos/rebisco.png',
+    showTitle: false,
+  },
+  {
+    name: 'Fresh',
+    img: '/logos/fresh.png',
+    showTitle: false,
+  },
+  {
+    name: 'Converse',
+    img: '/logos/converse.png',
+    showTitle: false,
+  },
+  {
+    name: 'Scotch Brite',
+    img: '/logos/scotch-brite.png',
+    showTitle: false,
+  },
+  {
+    name: 'Zesto',
+    img: '/logos/zesto.png',
+    showTitle: false,
+  },
+  {
+    name: 'plus!',
+    img: '/logos/plus.png',
+    showTitle: false,
+  },
+  {
+    name: 'Fresh Pick',
+    img: '/logos/freshpick.png',
+    showTitle: false,
+  },
+  {
+    name: 'Zesto Lodi',
+    img: '/logos/lodi.png',
+    showTitle: false,
+  },
+  {
+    name: 'Dogs and the City',
+    img: '/logos/dogs-and-city.png',
+    showTitle: false,
+  },
+  {
+    name: 'City of Pasig',
+    img: '/logos/pasig.png',
+    showTitle: false,
+  },
+  {
+    name: 'CDO Foodsphere, Inc.',
+    img: '/logos/cdo.png',
+    showTitle: false,
+  },
+  {
+    name: 'Catholic Educational Association of the Philippines',
+    img: '/logos/ceap.png',
+    showTitle: true,
+  },
+  {
+    name: 'MAPA',
+    img: '/logos/mapa.png',
+    showTitle: true,
+  },
+  {
+    name: 'Novuhair',
+    img: '/logos/novuhair.png',
+    showTitle: true,
+  },
+  {
+    name: 'Nacho King',
+    img: '/logos/nacho-king.png',
+    showTitle: true,
+  },
+  {
+    name: 'Delfi',
+    img: '/logos/delfi.png',
+    showTitle: false,
+  },
+  {
+    name: 'SariMonde',
+    img: '/logos/sarimonde.png',
+    showTitle: true,
+  },
+  {
+    name: 'Private Education Assistance Commitee',
+    img: '/logos/peac.png',
+    showTitle: true,
+  },
+  {
+    name: 'Soban',
+    img: '/logos/soban.png',
+    showTitle: false,
+  },
+  {
+    name: 'SibYulLee',
+    img: '/logos/sibyullee.png',
+    showTitle: true,
+  },
+  {
+    name: 'Cycles',
+    img: '/logos/cycles.png',
+    showTitle: false,
+  },
+  {
+    name: 'Cradle',
+    img: '/logos/cradle.png',
+    showTitle: false,
+  },
+  {
+    name: 'La Salle College - Antipolo',
+    img: '/logos/lsca.png',
+    showTitle: true,
+  },
+  {
+    name: 'Saint Paul University Manila',
+    img: '/logos/spcm.png',
+    showTitle: true,
+  },
+  {
+    name: 'Pasig Catholic College',
+    img: '/logos/pcc.png',
+    showTitle: true,
+  },
+  {
+    name: 'Paco Catholic School',
+    img: '/logos/pcs.png',
+    showTitle: true,
+  },
+  {
+    name: 'Malate Catholic School',
+    img: '/logos/mcs.png',
+    showTitle: true,
+  },
+  {
+    name: 'Saint Joseph School of Pandacan',
+    img: '/logos/sjsp.png',
+    showTitle: true,
+  },
+  {
+    name: 'Santa Clara Parish School',
+    img: '/logos/scps.png',
+    showTitle: true,
+  },
+  {
+    name: 'Our Lady of Peace School',
+    img: '/logos/olps.png',
+    showTitle: true,
+  },
+  {
+    name: 'Good Shepherd Catholic School',
+    img: '/logos/gscs.png',
+    showTitle: true,
+  },
+  {
+    name: 'Claret School',
+    img: '/logos/claret.png',
+    showTitle: true,
+  },
+  {
+    name: 'Saint Piux X Parochial School',
+    img: '/logos/spps.png',
+    showTitle: true,
+  },
+  {
+    name: 'Ermita Catholic School',
+    img: '/logos/ecs.png',
+    showTitle: true,
+  },
+];
